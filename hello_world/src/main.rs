1636516194d2adaaf0f985a34fc88ef9e6015374
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-05-25 22:57:28 +0700

    function

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,24 +1,22 @@
-// Derive the `fmt::Debug` implementation for `Structure`. `Structure`
-// is a structure which contains a single `i32`.
-#[derive(Debug)]
-struct Structure(i32);
-
-// Put a `Structure` inside of the structure `Deep`. Make it printable
-// also.
-#[derive(Debug)]
-struct Deep(Structure);
 fn main() {
-    // Printing with `{:?}` is similar to with `{}`.
-    println!("{:?} months in a year.", 12);
+    println!("{}", with_static_return_int());
+    println!("{}", with_param_int(20));
+    println!("{}", with_param_string("Hello Rust!"));
+    println!("{}", with_static_return_string())
+}
 
-    println!("{1:?} {0:?} is the {actor:?} name.",
-        "Slater",
-        "Christian",
-        actor="actor's");
+fn with_static_return_int() -> i8 {
+    return 20;
+}
 
-    println!("Now {:?} will print!", Structure(3));
+fn with_param_int(int: i8) -> i8 {
+    return int*2;
+}
 
-    // The problem with `derive` is there is no control over how
-    // the results look. What if I want this to just show a `7`?
-    println!("Now {:?} will print!", Deep(Structure(7)));
+fn with_param_string(text: &str) -> &str {
+    return text;
+}
+
+fn with_static_return_string() -> &'static str {
+    return "Hello Rust with Static String";
 }
